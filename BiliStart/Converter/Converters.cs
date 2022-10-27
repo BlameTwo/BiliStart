194ce2b7f90dd4d9b1198479d48390de1016d92c
一个渣渣 <65759143+BlameTwo@users.noreply.github.com>
2022-10-27 15:17:53 +0800

    更新分区排行榜

---
+++ BiliStart/Converter/Converters.cs
@@ -0,0 +1,29 @@
+﻿using BilibiliAPI;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace BiliStart.Converter
+{
+    internal class CidConverters : IValueConverter
+    {
+        CidFormat Format = new CidFormat();
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is Cid cid)
+            {
+                return Format.FromEnum(cid);
+            }
+            return "~什么也没有";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BiliStart/ViewModel/TopVideoVM.cs
+++ BiliStart/ViewModel/TopVideoVM.cs
@@ -1,5 +1,7 @@
-﻿using BiliBiliAPI.Models.TopList;
+﻿using BilibiliAPI;
+using BiliBiliAPI.Models.TopList;
 using BiliBiliAPI.Models.Videos;
+using BiliStart.Converter;
 using BiliStart.Windows;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
@@ -19,6 +21,34 @@ namespace BiliStart.ViewModel
             IsActive = true;
             Loaded = new RelayCommand(() => loaded());
             Selected = new RelayCommand<TopVideo>((arg) => selected(arg));
+            Search = new RelayCommand<CidModel>((arg) => search(arg));
+            _Cids = new ObservableCollection<CidModel>();
+            _Cids.Add(new CidModel() { cid = Cid.All});
+            _Cids.Add(new CidModel() { cid = Cid.Douga });
+            _Cids.Add(new CidModel() { cid = Cid.Chinese});
+            _Cids.Add(new CidModel() { cid = Cid.Music});
+            _Cids.Add(new CidModel() { cid = Cid.Dance });
+            _Cids.Add(new CidModel() { cid = Cid.Game });
+            _Cids.Add(new CidModel() { cid = Cid.knowledge });
+            _Cids.Add(new CidModel() { cid = Cid.Tech });
+            _Cids.Add(new CidModel() { cid = Cid.Sports });
+            _Cids.Add(new CidModel() { cid = Cid.Car  });
+            _Cids.Add(new CidModel() { cid = Cid.Life  });
+            _Cids.Add(new CidModel() { cid = Cid.Food  });
+            _Cids.Add(new CidModel() { cid = Cid.Animal  });
+            _Cids.Add(new CidModel() { cid = Cid.Kichiku  });
+            _Cids.Add(new CidModel() { cid = Cid.Fashion  });
+            _Cids.Add(new CidModel() { cid = Cid.Ent  });
+            _Cids.Add(new CidModel() { cid = Cid.Documentary  });
+            _Cids.Add(new CidModel() { cid = Cid.Movie  });
+            _Cids.Add(new CidModel() { cid = Cid.TV  });
+
+        }
+
+        private async void search(CidModel? arg)
+        {
+            var list = await TopListVideo.GetTopVideo(arg!.cid, 7);
+            List = list.Data.List.ToObservableCollection();
         }
 
         private void selected(TopVideo? arg)
@@ -42,7 +72,35 @@ namespace BiliStart.ViewModel
             set=>SetProperty(ref _List, value);
         }
 
+        private ObservableCollection<CidModel> Cids;
+
+        public ObservableCollection<CidModel> _Cids
+        {
+            get { return Cids; }
+            set => SetProperty(ref Cids, value);
+        }
+
+
+
         public RelayCommand Loaded { get; private set; }
         public RelayCommand<TopVideo> Selected { get; set; }
+        public RelayCommand<CidModel> Search { get; private set; }
+    }
+
+    public class CidModel
+    {
+        CidFormat Cid = new CidFormat();
+        public string DisplayName
+        {
+            get
+            {
+                return Cid.FromEnum(cid);
+            }
+        }
+
+        public Cid cid
+        {
+            get;set;    
+        }
     }
 }
